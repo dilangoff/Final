31f3f76cdd87bf781dbfce7f465e0f92e5f8bee1
Dilan Goff <02dilan.goff@gulllakecs.org>
2019-03-14 08:20:07 -0400

    updated

--- Final Exam.py
+++ Final Exam.py
@@ -3,35 +3,10 @@ Hi my name is Dilan Goff and this is my finale exam project I made a drawing wit
 reviewers about it
 '''
 
-
-
-
-
-import turtle
-
-my_turtle = turtle.Turtle()
-
-# Python program to draw
-# Rainbow Benzene
-# using Turtle Programming
-import turtle
-
-t.speed(0)
-colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
-t = turtle.Pen()
-turtle.bgcolor('black')
-for x in range(180):
-    t.pencolor(colors[x%6])
-    t.width(x/100 + 1)
-    t.forward(x)
-    t.left(59)
-
-turtle.done()
-
 #This is my code for the survey questions
 
-    like = -5
- while like <1 or like >3:
+like = -5
+while like <1 or like >3:
     try:
         like = float(input('Did you like my Drawing?(1 = yes 2 = No) '))
 
